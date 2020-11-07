0c88e3ab767b0287ddacede564cd4c8d64afc8ec
Erni <38285979+arnaujc91@users.noreply.github.com>
2020-11-07 19:39:59 +0100

    Update fastai2onAzureSpotDSVM.sh

--- fastai2onAzureSpotDSVM.sh
+++ fastai2onAzureSpotDSVM.sh
@@ -12,7 +12,7 @@ read -s -p "Re-enter Password: " password2
 echo
 done
 
-az group create --name $vmname -l westus2
+az group create --name $vmname -l eastus
 
 echo "Creating Azure Data Science VM $vmname..."
 # You can change the size parameter if you want something other than NVidia K80 GPU instance. 
